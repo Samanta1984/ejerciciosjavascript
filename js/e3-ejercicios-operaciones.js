338d2b2880a10f52f43689ef81c49de0cd1e6098
Samanta1984 <samantaramos1984@hotmail.com>
2024-05-02 15:29:44 +0200

    operaciones-ejercicios

---
+++ js/e3-ejercicios-operaciones.js
@@ -0,0 +1,153 @@
+//HACER EJERCICIOS DEL TEMA 3. OPERACIONES
+//CONSTANTES
+
+//Declara las siguientes variables o constantes según corresponda:
+//Número de meses del año.
+//Número de días de la semana.
+//Número de habitantes en tu ciudad.
+//Edad de tu cantante favorito.
+//Número de estaciones del año.
+//Nombre de tu ciudad.
+//Número de horas en un día.
+//Precio de la gasolina.
+//Número de libros que has leído.
+
+
+const MESES_DEL_ANO = 12;
+const DIAS_DE_LA_SEMANA = 7;
+let habitantes_en_mi_ciudad = 1000000; 
+let edad_de_mi_cantante_favorito = 35; 
+const ESTACIONES_DEL_ANO = 4;
+let nombre_de_mi_ciudad = "Malaga"; 
+const HORAS_EN_UN_DI = 24;
+let precio_de_la_gasolina = 1.49; 
+let libros_leidos = 50; 
+
+document.write("<pre>");
+document.write("CONSTANTES\n\n");
+
+document.write("Meses del ano = 12\n");
+document.write("Dias de la semana = 7\n");
+document.write("Habitantes en mi ciudad = 1000000\n");
+document.write("Edad de mi cantante favorito = 35\n");
+document.write("Estaciones del ano = 4\n");
+document.write("Nombre de mi ciudad = Malaga\n");
+document.write("Horas en un dia = 24\n");
+document.write("Precio de la gasolina = 1.49\n");
+document.write("Libros leidos = 50\n");
+
+document.write("</pre>");
+
+
+
+
+//3. OPERACIONES
+
+//Haz los siguientes ejercicios en el fichero javascript e3-ejercicios-guardar-informacion.js y muestralos en la pagina e3-guardar-informacion.html junto con los ejemplos vistos en clase.
+
+//Crea variables para guardar el nombre de los siguientes productos junto con sus precios por kilo y muestralos en tu pagina. 
+
+//Manzana: 2,5
+let Manzana = 2.5;
+
+//Pera: 1,5
+let Pera = 1.5;
+
+//Platano: 1,8
+let Platano = 1.8;
+//let precioTotal;
+
+//A continuación muestra por pantalla el precio total de:
+
+//1 kilo de manzana y 1 kilo de pera
+const precioManzana = 2.5;
+const precioPera = 1.5;
+
+let kiloManzana = 1;
+let kiloPera = 1;
+let precioTotal = (kiloManzana * precioManzana) + (kiloPera * precioPera);
+
+        
+document.write("El precio total de 1 kilo de pera y 1 kilo de manzana es: €" + precioTotal.toFixed(2));
+
+//1 kilo de platano y 1 kilo de pera
+
+//2 kilos de peras
+
+//3 kilos de platanos
+
+//2 kilos de manzana y 3 kilos de peras
+
+//1,5 kilos de platano, 2 kilos de peras, 3 kilos de manzana
+
+//2,5 kilos de peras, 3,5 kilos de platano
+
+//Crea variables para guardar las notas de un alumno: 7.5, 10.5, 8, 9.5, 7 y muestra su nota media.
+
+//Calcula el área de un cuadrado con lado de longitud 5 cm.
+
+//Encuentra el área de un rectángulo con dimensiones de 8 cm de largo y 6 cm de ancho.
+
+//Determina el área de un triángulo con base de 10 metros y altura de 8 metros.
+
+//Calcula el área de un círculo con radio de 4 centímetros
+
+//Encuentra el área de un trapecio con bases de longitudes 6 cm y 10 cm, y altura de 8 cm.
+
+//Determina el área de un rombo con diagonales de longitudes 12 cm y 16 cm.
+
+//Calcula el área de un paralelogramo con base de 7 metros y altura de 9 metros.
+
+//Encuentra el área de un pentágono regular con lado de 6 centímetros.
+
+
+///4. CADENAS (TEXTOS)
+
+//Haz los siguientes ejercicios en el fichero javascript e4-cadenas.js y muestralos en la pagina e4-cadenas.html junto con los ejemplos vistos en clase. No uses document.write para mostrar la solución. En su lugar, agrega por cada ejercicio a tu html un título por cada ejercicio y un parrafo o lista con ids para mostrar cada solución en su lugar.
+
+
+//Crea una variable con la frase Hola que tal, bienvenida a QA, y muestrala junto con:
+
+//Su longitud
+
+//La posición de la palabra QA
+
+//La primera letra de la frase.
+
+//La última letra de la frase.
+
+//Muestra la frase completa en mayúscula
+
+//Muestra la frase completa en minúscula
+
+//Crea una variable con tu nombre, y modifica la frase para que diga Hola seguido de tu nombre
+
+//Crea una nueva palabra, con la misma frase quitando la palabra que tal.
+
+//Crea una variable con una frase en la que te presentes y muestrala junto con:
+
+//Su longitud
+
+//La posición de tu nombre
+
+//La primera letra de la frase.
+
+//La última letra de la frase.
+
+//Muestra la frase completa en mayúscula
+
+//Muestra la frase completa en minúscula
+
+//Muestra la frase cambiando tu nombre por otro que elijas.
+
+//Crea una nueva palabra, con la misma frase quitando tu nombre.
+
+//Crea una variable con tu nombre completo y muestralo por pantalla. A continuación, quita los espacios de la variable y muestralo por pantalla.
+
+//Crea una variable con una palabra en minuscula y muestralo por pantalla. Cambia la primera letra a mayuscula y muestrala por pantalla.
+
+//Crea una cadena con el texto Hola Mundo y muestralo por pantalla. Reemplaza la palabra Hola por Adios y muestralo por pantalla.
+
+
+
+
